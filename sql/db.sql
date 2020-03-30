c288d66b1bacfa5b29cacba31a688231241516da
张 悦 <yue.zhang@shuyun.com>
2020-03-30 22:41:38 +0800

    增加商品业务代码

--- sql/db.sql
+++ sql/db.sql
@@ -10,7 +10,6 @@ CREATE TABLE `user_info`  (
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息';
 
-
 drop table if exists `user_password`;
 CREATE TABLE `user_password`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
@@ -18,3 +17,22 @@ CREATE TABLE `user_password`  (
   `encrypt_password` varchar(128) NOT NULL DEFAULT '' COMMENT '加密的密码',
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户密码表';
+
+drop table if exists `item`;
+CREATE TABLE `item`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `title` varchar(64) NOT NULL DEFAULT '' COMMENT '商品标题',
+  `price` decimal(8,2) NOT NULL DEFAULT 0 COMMENT '价格',
+  `description` varchar(500) NOT NULL DEFAULT '' COMMENT '描述',
+  `sales` int(11) NOT NULL DEFAULT 0 COMMENT '销量',
+  `img_url` varchar(128) NOT NULL DEFAULT '' COMMENT '图片URL',
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+drop table if exists `item_stock`;
+CREATE TABLE `item_stock`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `stock` int(11) NOT NULL DEFAULT 0 COMMENT '库存',
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品库存表';
